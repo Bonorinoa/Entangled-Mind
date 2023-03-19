7e4664a34dcc35447656fbe71ea04601ce9691b4
Augusto Gonzalez Bonorino <Augusto.GonzalezBonorino1@marist.edu>
2023-03-18 21:53:22 -0700

    cleaned code and added token plus cost counters

--- Streamlit_GPT4/course_outlines.py
+++ Streamlit_GPT4/course_outlines.py
@@ -1,35 +1,52 @@
 import openai
 import streamlit as st
-import pandas as pd
-import numpy as np
+
 
 openai.api_key = "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"
 
-# Define function to generate an outline
-def generate_outline_GPT(context, engine, max_tokens, temperature):
-    prompt = f"Generate an outline for an online course on {context}"
-    response = openai.Completion.create(
-        engine=engine,
-        prompt=prompt,
-        max_tokens=max_tokens,
-        n=1,
-        stop=None,
-        temperature=temperature,
-    )
-    outline = response.choices[0].text.strip()
-    return outline
 
-def generate_outline_chatGPT(context, model, max_tokens, temperature):
+def compute_cost(tokens, engine):
+    
+    model_prices = {"text-davinci-002": 0.02, 
+                    "gpt-3.5-turbo": 0.002, 
+                    "gpt-4": 0.03}
+    model_price = model_prices[engine]
+    
+    if engine == "text-davinci-002":
+        cost = (tokens / 1000) * model_price
+    else:
+        cost = (tokens / 1000) * model_price
+    return cost
+
+
+def generate_outline(context, engine, max_tokens, temperature):
     prompt = f"Generate an outline for an online course on {context}"
-    response = openai.ChatCompletion.create(
-        model=model,
+    
+    if st.session_state.engine == "text-davinci-002":
+        prompt = f"Generate an outline for an online course on {context}"
+        response = openai.Completion.create(
+            engine=engine,
+            prompt=prompt,
+            max_tokens=max_tokens,
+            n=1,
+            stop=None,
+            temperature=temperature,
+        )
+        outline = response.choices[0].text.strip()
+        tokens_used = response.usage.total_tokens
+        
+    else:
+        response = openai.ChatCompletion.create(
+        model=engine,
         messages=[{"role": "system", "content": "You are a helpful teaching assistant who is skilled in drafting outlines, both in English and Spanish, for succesful online courses."},
                   {"role": "user", "content": prompt}],
         max_tokens=max_tokens,
         temperature=temperature,
-    )
-    outline = response['choices'][0]['message']['content'].strip()
-    return outline
+        )
+        outline = response['choices'][0]['message']['content'].strip()
+        tokens_used = response.usage.total_tokens
+        
+    return (outline, tokens_used)
 
 # Define session state variables
 if "max_tokens" not in st.session_state:
@@ -38,6 +55,8 @@ if "temperature" not in st.session_state:
     st.session_state.temperature = 0.5
 if "engine" not in st.session_state:
     st.session_state.engine = "text-davinci-002"
+if "tokens_used" not in st.session_state:
+    st.session_state.tokens_used = 0
 
 # Set app title and instructions
 st.title("Online Course Outline Generator")
@@ -51,36 +70,29 @@ with st.sidebar:
     st.session_state.engine = st.selectbox("Engine", 
                                            ["text-davinci-002", "gpt-3.5-turbo", "gpt-4"])
 
+    st.write("## Tokens Used")
+    st.write(st.session_state.tokens_used)
+    st.info(f"So far spent: ${compute_cost(st.session_state.tokens_used, st.session_state.engine)}")
+    
 # Get user input and generate outline
 context = st.text_input("Enter context for your course here:")
 if context:
     try:
         if st.button("Generate Outline"):
-            if st.session_state.engine == "text-davinci-002":
-                outline = generate_outline_GPT(context, 
-                                            st.session_state.engine, 
-                                            st.session_state.max_tokens, 
-                                            st.session_state.temperature)
-                
-            elif st.session_state.engine == "gpt-3.5-turbo":
-                outline = generate_outline_chatGPT(context, 
-                                                st.session_state.engine, 
-                                                st.session_state.max_tokens, 
-                                                st.session_state.temperature)
-                
-            elif st.session_state.engine == "gpt-4":
-                outline = generate_outline_chatGPT(context, 
-                                                st.session_state.engine, 
-                                                st.session_state.max_tokens, 
-                                                st.session_state.temperature)
 
-            st.write("## Generated Outline")
-            st.write(outline)
+            outline, tokens_used = generate_outline(context, 
+                                        st.session_state.engine, 
+                                        st.session_state.max_tokens, 
+                                        st.session_state.temperature)
             
-    except openai.Error as error:
-        st.write("An error occurred while generating the outline:")
-        st.write(error.message)
-        st.write("Please try again later or adjust the model parameters.")
+            st.session_state.tokens_used += tokens_used
+            st.write("## Generated Outline")
+            st.write(f" ---- OUTLINE GENERATED ---- \n\n {outline}")
+            st.info(f"Tokens used: {tokens_used}. Total tokens used: {st.session_state.tokens_used}")
+            st.info(f"Total cost: ${compute_cost(st.session_state.tokens_used, st.session_state.engine)}")
+            
+    except Exception as e:
+        st.error(e)
         
 if not context:
     st.info("Please enter some context for your online course in the text box")
